00fb691decd3d8018d3134f923fa72b03691cb55
hanif zekri Astaneh <111789255+hanifzekri@users.noreply.github.com>
2024-12-04 18:30:31 +0330

    Update digidargah.php

--- digidargah.php
+++ digidargah.php
@@ -62,8 +62,8 @@ function wc_gateway_digidargah_init(){
 
                 $this->order_status = $this->get_option('order_status');
 
-                $this->payment_endpoint = 'https://digidargah.com/action/ws/request_create';
-                $this->verify_endpoint = 'https://digidargah.com/action/ws/request_status';
+                $this->payment_endpoint = 'https://digidargah.com/action/ws/request/create';
+                $this->verify_endpoint = 'https://digidargah.com/action/ws/request/status';
 
                 $this->success_message = $this->get_option('success_message');
                 $this->failed_message = $this->get_option('failed_message');
